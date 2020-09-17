347ef6a7a8c7c2e9277a05fb658ecfca2db8482b
Kim Nguyen <nnhk23@gmail.com>
2020-09-17 19:05:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,4 @@
 require 'pry'
 def reverse_each_word(string)
   reversed = []
-  result = []
-  array = string.split("")
-  array.each {
-    |element| element.length.times{reversed << element.split("").shift}
-  }
-  result.join
-  binding.pry
-end
+  
