15360e2572ad1c00a5c51fa19c52a415ff5eabd5
Kim Nguyen <nnhk23@gmail.com>
2020-09-17 18:52:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ require 'pry'
 def reverse_each_word(string)
   reversed = []
   array = string.split("")
-  binding.pry
   array.each {
     |element| reversed << element.split("")
   }
+  binding.pry
   reversed.join
 end
