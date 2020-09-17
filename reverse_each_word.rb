40c72337ad576ec9ad545a3186b187066a6161e4
Kim Nguyen <nnhk23@gmail.com>
2020-09-17 19:27:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,14 +6,3 @@ def reverse_each_word(string)
   end
   result.join(" ")
 end
-
-def reverse_word (string)
-  result = []
-  split_string = string.split(" ")
-  split_string.collect do |element|
-    result << element.reverse 
-  end
-  result.join(" ")
-end
-
-reverse_each_word("Hello there, and how are you?")
