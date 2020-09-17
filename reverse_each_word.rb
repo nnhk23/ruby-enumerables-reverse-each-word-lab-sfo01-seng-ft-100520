74120ffd5787f8b22d10a515caf762d3d86676e5
Kim Nguyen <nnhk23@gmail.com>
2020-09-17 19:07:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def reverse_each_word(string)
   result = []
-  split_string = string.split("")
+  split_string = string.split(" ")
   split_string.each do |element|
     result << element.reverse
   end
